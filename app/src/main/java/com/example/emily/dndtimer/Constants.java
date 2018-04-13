bdce998b68bc1b79abfcc617753ab9c4f52473cc
Emily K. Gehman <egehman@gmail.com>
2018-04-13 14:22:05 -0700

    Save functionality kind of works; consolidated adapters

--- app/src/main/java/com/example/emily/dndtimer/Constants.java
+++ app/src/main/java/com/example/emily/dndtimer/Constants.java
@@ -2,4 +2,5 @@ package com.example.emily.dndtimer;
 
 public class Constants {
     public static final String KEY_PLAYERS = "PLAYERS";
+    public static final String FILE_PLAYERS = "players.txt";
 }
